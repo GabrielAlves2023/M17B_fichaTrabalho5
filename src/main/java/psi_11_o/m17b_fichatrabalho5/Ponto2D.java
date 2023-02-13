e9725d2f1fe48580b9891b9b5b5676be88dec733
Gabriel Alves <a22235@d1-08-04.dped.local>
2023-02-13 16:27:11 +0000

    Revert "Ficha de trabalho 5 - Criação do repositório local."
    This reverts commit d345b22ea39b899c246c4f5c22f1f04e4ce2913e.

--- src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
@@ -1,9 +1,5 @@
 package psi_11_o.m17b_fichatrabalho5;
 
-/**
- * @author Gabriel Alves
- * @version 1.0.1
- */
 public class Ponto2D {
     private int x;
     private int y;
@@ -18,40 +14,12 @@ public class Ponto2D {
         this.y = y;        
     }
     
-    /**
-     * @return the x
-     */
-    public int getX() {
-        return x;
-    }
-
-    /**
-     * @param x the x to set
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
-     * @return the y
-     */
-    public int getY() {
-        return y;
-    }
-
-    /**
-     * @param y the y to set
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
-    
     public double distancia(Ponto2D a){
-        return Math.abs(Math.sqrt(Math.pow((getX()-a.getX()),2)+Math.pow((getY()-a.getY()),2)));
+        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
     }
 
     @Override
     public String toString() {
-        return "Ponto2D[" + getX() + "," + getY() + "]";
-    } 
+        return "Ponto2D[" + x + "," + y + "]";
+    }    
 }
